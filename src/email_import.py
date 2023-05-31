e6c4a80e7a998337c447a4dd749a764a87a3112f
Christoph Backhaus <christoph.backhaus@nadooit.de>
2023-05-31 12:48:11 +0200

    email_imoport and O365 integration

--- src/email_import.py
+++ src/email_import.py
@@ -1,4 +1,4 @@
-from O365 import Account
+from O365 import Account, FileSystemTokenBackend
 import os
 from dotenv import load_dotenv
 
@@ -8,12 +8,29 @@ load_dotenv()
 # Get the credentials from the environment variables
 credentials = (os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET"))
 
-account = Account(credentials)
-if account.authenticate(scopes=["basic", "calendar_all"]):
-    print("Authenticated!")
+# Specify token path (using FileSystemTokenBackend for simplicity here)
+token_backend = FileSystemTokenBackend(token_path=".", token_filename="o365_token.txt")
 
+# Instantiate the Account object
+account = Account(credentials, token_backend=token_backend)
+
+# Check if we are authenticated
+if not account.is_authenticated:
+    # Authenticate here
+    account.authenticate(scopes=["basic", "calendar_all"])
+
+# Instantiate the mailbox object
 mailbox = account.mailbox()
+
+# Access the Inbox
 inbox = mailbox.inbox_folder()
 
-for message in inbox.get_messages():
-    print(message)
+
+def import_emails():
+    # Get messages from the Inbox
+    for message in inbox.get_messages():
+        print(message)
+
+
+# Call the function
+import_emails()
